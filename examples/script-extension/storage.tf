608b188617c775ad6fc70db5841d4d5d104265b6
Jorrit05 <jorstut@hotmail.com>
2023-10-09 15:42:25 +0200

    fix: tf fmt

--- examples/script-extension/storage.tf
+++ examples/script-extension/storage.tf
@@ -27,28 +27,28 @@ module "storage" {
 }
 
 resource "azurerm_role_assignment" "vmss_to_storage" {
-  principal_id   = module.vmss.vmss.identity.0.principal_id
+  principal_id         = module.vmss.vmss.identity.0.principal_id
   role_definition_name = "Storage Blob Data Contributor"
-  scope          = "${module.storage.account.id}/blobServices/default/containers/${module.storage.containers.scripts.name}"
+  scope                = "${module.storage.account.id}/blobServices/default/containers/${module.storage.containers.scripts.name}"
 }
 
 resource "azurerm_storage_blob" "install_script" {
-    name                   = "createfile"
-    storage_account_name   = module.storage.account.name
-    storage_container_name = module.storage.containers.scripts.name
-    type                   = "Block"
-    source                 = "${path.module}/createfile.sh"
+  name                   = "createfile"
+  storage_account_name   = module.storage.account.name
+  storage_container_name = module.storage.containers.scripts.name
+  type                   = "Block"
+  source                 = "${path.module}/createfile.sh"
 
-    # Ensures that the blob will be updated if the source file changes.
-    content_md5            = filemd5("${path.module}/createfile.sh")
+  # Ensures that the blob will be updated if the source file changes.
+  content_md5 = filemd5("${path.module}/createfile.sh")
 }
 
 
 # https://learn.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux#property-values
 resource "azurerm_virtual_machine_scale_set_extension" "script_extension" {
   depends_on = [
-        azurerm_storage_blob.install_script
-    ]
+    azurerm_storage_blob.install_script
+  ]
 
   name                         = "script-extension"
   virtual_machine_scale_set_id = module.vmss.vmss.id
@@ -57,9 +57,9 @@ resource "azurerm_virtual_machine_scale_set_extension" "script_extension" {
   type_handler_version         = "2.1"
 
   protected_settings = jsonencode({
-      "fileUris" = ["${azurerm_storage_blob.install_script.url}"],
-      "commandToExecute" = "mv createfile createfile.sh; chmod +x createfile.sh; sh createfile.sh ; ls -al",
-      "managedIdentity" : { }
+    "fileUris"         = ["${azurerm_storage_blob.install_script.url}"],
+    "commandToExecute" = "mv createfile createfile.sh; chmod +x createfile.sh; sh createfile.sh ; ls -al",
+    "managedIdentity" : {}
     }
   )
 }
